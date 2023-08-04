913312f7e3aa3d9970216a826e8428bac2f55777
Dallin Breen <dallinbreen624@gmail.com>
2023-08-03 21:54:33 -0600

    Updated some code for the adding to the list modal

--- src/components/AddData.js
+++ src/components/AddData.js
@@ -10,6 +10,19 @@ import {
 } from "react-native";
 
 export default function AddData({ visible, close }) {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("$ 0.00");
+
+  function handleTitle(text) {
+    setTitle(text);
+  }
+
+  function handleAmount(text) {
+    let numericValue = text.replace(/[^0-9.,]/g, "");
+    const valueWithSign = "$ " + numericValue;
+    setAmount(valueWithSign);
+  }
+
   return (
     <Modal visible={visible} animationType="slide" transparent={true}>
       <View style={styles.container}>
@@ -17,6 +30,41 @@ export default function AddData({ visible, close }) {
           <Pressable onPress={close}>
             <Text>Close Modal</Text>
           </Pressable>
+          <View style={styles.inputs}>
+            <Text style={{ fontSize: 17, fontWeight: "bold" }}>Title</Text>
+            <TextInput
+              style={{
+                height: "60%",
+                width: "100%",
+                borderWidth: 2,
+                borderColor: "#747474",
+                paddingHorizontal: 10,
+                backgroundColor: "white",
+                borderRadius: 50,
+              }}
+              keyboardType="default"
+              value={title}
+              onChangeText={handleTitle}
+            />
+          </View>
+          <View style={styles.inputs}>
+            <Text style={{ fontSize: 17, fontWeight: "bold" }}>Amount</Text>
+            <TextInput
+              style={{
+                height: "60%",
+                width: "100%",
+                borderWidth: 2,
+                borderColor: "#747474",
+                paddingHorizontal: 10,
+                backgroundColor: "white",
+                borderRadius: 50,
+              }}
+              keyboardType="numeric"
+              value={amount}
+              returnKeyType="done"
+              onChangeText={handleAmount}
+            />
+          </View>
         </View>
       </View>
     </Modal>
@@ -37,5 +85,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#D9D9D9",
     flexDirection: "column",
     alignItems: "center",
+  },
+  inputs: {
+    width: "75%",
+    height: "10%",
+    marginBottom: "5%",
+    justifyContent: "space-between",
   },
 });
